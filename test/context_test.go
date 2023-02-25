22465819a990137a8c7b3d70bfac61cb9d589c63
26n119e <yuxin.weng@outlook.com>
2023-02-25 14:51:55 +0800

    chore: tested the common methods under the context package.

--- test/context_test.go
+++ test/context_test.go
@@ -1,4 +1,4 @@
-package main
+package test
 
 import (
 	"fmt"
@@ -10,9 +10,9 @@ import (
 // The provided key myst be comparable and should not be of type string or any other built-in type to avoid collisions between packages using context.
 type contextKey string
 
-func TestContext(t *testing.T) {
-	ctx := context.Background()
-	// ctx := context.TODO()
+func TestWithValue(t *testing.T) {
+	// ctx := context.Background()
+	ctx := context.TODO()
 
 	ctxKey := contextKey("test")
 	ctx = context.WithValue(ctx, ctxKey, "test val")
@@ -26,5 +26,68 @@ func TestContext(t *testing.T) {
 	}()
 
 	time.Sleep(time.Second * 2)
+}
+
+func TestWithCancel(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func(cancel context.CancelFunc) {
+		time.Sleep(time.Second*2)
+		cancel()
+	}(cancel)
+
+	select {
+	case <-ctx.Done():
+		fmt.Printf("%s\n", "use cancel function.")
+	}
+
+}
+
+func TestWithTimeout(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+
+		done := make(chan struct{})
+
+		go func(done chan struct{}, sleepTime int) {
+			time.Sleep(time.Duration(sleepTime) * time.Second)
+			done <- struct{}{}
+		}(done, i)
+
+		go func(ctx context.Context, done chan struct{}) {
+			select {
+			case <-ctx.Done():
+				fmt.Printf("ctx timeout\n")
+			case <-done:
+				fmt.Printf("work done\n")
+			}
+		}(ctx, done)
+	}
+
+	time.Sleep(3*time.Second)
+
+}
+
+func TestWithDeadline(t *testing.T) {
+	deadline := time.Now().Add(5 * time.Second)
+	ctx, cancel := context.WithDeadline(context.Background(), deadline)
+	defer cancel()
+
+	done := make(chan struct{})
+
+	go func(done chan struct{}) {
+		// do something
+		time.Sleep(4*time.Second)
+		done <- struct{}{}
+	}(done)
+
+	select {
+	case <-ctx.Done():
+		fmt.Printf("deadline\n")
+	case <-done:
+		fmt.Printf("work done\n")
+	}
+
 
 }
